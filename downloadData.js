0511ea30eec4e0075cf1d795237fe3d63b083647
bmsullivan <bmsullivan@gmail.com>
2020-04-29 23:03:21 -0500

    Pulling data with fetches

--- downloadData.js
+++ downloadData.js
@@ -1,7 +1,19 @@
-var hist = document.getElementById('History');
+(async() => {
+    var hist = document.getElementById('History');
 
-var rows = hist.getElementsByClassName('HistoryPanelRow');
+    var rows = hist.getElementsByClassName('HistoryPanelRow');
 
-var content = rows.toString();
+    let content = "Date,Sanctioning Number\n";
+    for (let row of rows) {
+        let dateElm = row.getElementsByClassName('HistoryPanelHeaderLabel Date')[0];
+        content += dateElm.innerHTML + ',';
+        let eventId = row.querySelector('a[data-type="Event"]').dataset.summarykey;
+        let eventResp = await fetch(`https://www.wizards.com/Magic/PlaneswalkerPoints/JavaScript/GetEventSummary/${eventId}`, {method: 'POST'});
+        let eventData = await eventResp.json();
+        let eventDetailElm = document.createElement('div');
+        eventDetailElm.innerHTML = eventData.Data.Value;
+        content += eventDetailElm.getElementsByClassName('EventSanctionNumber')[0].innerText + '\n';
+    }
 
-chrome.runtime.sendMessage({content: content});
+    chrome.runtime.sendMessage({content: content});
+})();
