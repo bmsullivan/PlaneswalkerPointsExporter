9ba41fc8441e32718f0051e4ab6ccf2bd7da5a0c
bmsullivan <bmsullivan@gmail.com>
2020-05-02 16:01:02 -0500

    Remove commented out code

--- popup.js
+++ popup.js
@@ -1,13 +1,4 @@
 let downloadButton = document.getElementById('downloadButton');
-// let link = document.getElementById('ppLink');
-
-// link.onclick = function (element) {
-//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//       chrome.tabs.executeScript(
-//           tabs[0].id,
-//           {code: 'window.location = "https://www.wizards.com/Magic/PlaneswalkerPoints/History#type=EventsOnly"'});
-//     });
-// };
 
 downloadButton.onclick = function(element) {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
